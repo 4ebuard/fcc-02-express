2a69cd2b4ec246dcd000c44114ba818f16d451b1
4ebuard <yartspetr@gmail.com>
2023-06-03 16:03:16 +0300

    added res.json

--- myApp.js
+++ myApp.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
     res.sendFile(htmlPath + '/index.html')
 })
 
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"})
+})
+
 console.log('Hello World');
 
 
