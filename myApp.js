dd38245cc1f279e16632e5cf84ea88945641288b
petrsmolianinov <46758888+4ebuard@users.noreply.github.com>
2023-06-06 00:27:02 +0300

    tryed fakeBin wuth query

--- myApp.js
+++ myApp.js
@@ -79,6 +79,24 @@ app.get('/name', (req, res) => {
     
 })    
         
+function fakeBin(x){
+    var z = x.split('')
+    var bin = [];
+    for (var i = 0; i < z.length; i++) {
+        if (z[i] < 5) {
+            bin.push(0)
+        } else {
+            bin.push(1)
+        }
+    }
+    return bin.join("");
+}
+
+app.get('/fakeBin', (req, res) => {
+    let x = req.query.num
+    res.send(fakeBin(x))
+})
+
 console.log('Hello World');
 
 
